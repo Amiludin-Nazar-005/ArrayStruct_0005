274a03cc6e849f78cc2d9c9d57c0a0d35ad42d54
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-04-14 11:56:59 +0700

    struck 4

---
+++ struck4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct player{
+    string name;
+    string id;
+    string level;
+    Weapon weapon;
+};
+struct Weapon{
+    string name;
+    int damage;
+};
+
+
+int main(){
+    player pl[3];
+    for(int i=0; i<3; i++){
+
+        cout << "masukkan nama player= " ;
+        getline(cin,pl[i].name) ;
+        cout << "masukkan id= " ;
+        cin >> pl[i].id ;
+        cout << "masukkan level= " ;
+        cin >> pl[i].level ;
+        cin.ignore();
+        cout << "masukkan nama weapon= ";
+        getline(cin, pl[i].weapon.name);
+        cout << "masukkan damage= ";
+        cin >> pl[i].weapon.damage;
+    }
+
+    for(int i=0; i<3; i++){
+
+        cout << "tampilkan player" << endl;
+        cout << "nama player= " << pl[i].name << endl;
+        cout << "id player= " << pl[i].id << endl;
+        cout << "level player= " << pl[i].level << endl;
+        cout << "nama weapon= " << pl[i].weapon.name << endl;
+        cout << "damage weapon= " << pl[i].weapon.damage << endl;
+    }
+}
